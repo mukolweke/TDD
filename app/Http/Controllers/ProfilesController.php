a199e6f7a30200daa0900e4ecae728b7d57bd16c
mukolweke <michaelolukak@gmail.com>
2018-08-21 09:20:44 +0300

    Extracting-a-Controller-Query-to-the-Model

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
-use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
@@ -11,18 +11,7 @@ class ProfilesController extends Controller
     {
         return view( 'profiles.show', [
             'profileUser'=> $user,
-            'activities'=> \App\Activity::feed($user)
+            'activities'=> Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Support\Collection
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -26,11 +26,13 @@ class ProfilesTest extends TestCase
     /** @test */
     function profiles_display_all_threads_created_by_associated_user()
     {
+        $this->signIn();
+
         $user = create('App\User');
 
-        $thread = create('App\Thread',['user_id'=>$user->id]);
+        $thread = create('App\Thread',['user_id'=>auth()->id()]);
 
-        $this->get("/profiles/{$user->name}")
+        $this->get("/profiles/".auth()->user()->name)
             ->assertSee($thread->title)
             ->assertSee($thread->body);
     }
